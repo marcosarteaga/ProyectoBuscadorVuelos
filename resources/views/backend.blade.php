6878dfc4cdf378839bad24c58d194d3e7c2dc501
miguelarteaga <marteagavalle@gmail.com>
2019-05-09 17:46:18 +0200

    Ofertas

--- resources/views/backend.blade.php
+++ resources/views/backend.blade.php
@@ -37,9 +37,55 @@
                 <!-- hotel search -->
                 <div class="bhoechie-tab-content">
                     <center>
-                      <h1 class="glyphicon glyphicon-home" style="font-size:12em;color:#55518a"></h1>
-                      <h2 style="margin-top: 0;color:#55518a">Cooming Soon</h2>
-                      <h3 style="margin-top: 0;color:#55518a">Hotel Directory</h3>
+                      <h1 style="margin-top: 0;color:#55518a">OFERTAS</h1>
+                      <!--<h1 class="glyphicon glyphicon-home" style="font-size:12em;color:#55518a"></h1>-->
+                        <table class="table">
+                          <thead>
+                            <tr>
+                              <th scope="col"></th>
+                              <th scope="col">Imagen</th>
+                              <th scope="col"></th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr>
+                              <form method="POST" action="/backend" enctype="multipart/form-data">
+                                <th scope="row">OFERTA 1</th>
+                                <td>
+                                  <input  type="file" name="archivoMultimedia">
+                                </td>
+                                <td>
+                                  <input class="btn btn-primary" type="submit" value="Subir">
+                                </td>
+                              </form>
+                            </tr>
+                            <tr>
+                              <form method="POST" action="/backend" enctype="multipart/form-data">
+                                <th scope="row">OFERTA 2</th>
+                                <td>
+                                  <input  type="file" name="archivoMultimedia">
+                                </td>
+                                <td>
+                                  <input class="btn btn-primary" type="submit" value="Subir">
+                                </td>
+                              </form>
+                            </tr>
+
+                            <tr>
+                              <form method="POST" action="/backend" enctype="multipart/form-data">
+                                <th scope="row">OFERTA 3</th>
+                                <td>
+                                  <input  type="file" name="archivoMultimedia">
+                                </td>
+                                <td>
+                                  <input class="btn btn-primary" type="submit" value="Subir">
+                                </td>
+                              </form>
+                            </tr>
+
+
+                          </tbody>
+                        </table>
                     </center>
                 </div>
             </div>
