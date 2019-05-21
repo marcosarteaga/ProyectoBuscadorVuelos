f7f81330dfc18a1793a8faa7e8154fcfe53fb717
miguelarteaga <marteagavalle@gmail.com>
2019-05-21 18:41:28 +0200

    Arreglos

--- app/Http/Controllers/filtro.php
+++ app/Http/Controllers/filtro.php
@@ -8,7 +8,7 @@ class filtro extends Controller
 {
     public function buscar(Request $request){
         $busqueda = $request->input('busqueda');
-        $arrayClientes = DB::table('pasajeros')->where('Nombre','like','%'.$busqueda.'%')->orwhere('Apellidos','like','%'.$busqueda.'%')->paginate(1);
+        $arrayClientes = DB::table('pasajeros')->where('Nombre','like','%'.$busqueda.'%')->orwhere('PrimerApellido','like','%'.$busqueda.'%')->orwhere('SegundoApellido','like','%'.$busqueda.'%')->paginate(1);
 
     	return view('backend',array('arrayClientes'=> $arrayClientes));
 
